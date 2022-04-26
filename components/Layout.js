d8e10e9044d3e63ac647eb414715bcbf741196b4
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-26 23:26:19 +0530

    fixed links

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
-import { Container } from './styles/Container.styled';
+import { Container } from './styles/Container.js';
 
 function Layout({ children }) {
   return (
--- components/Links.js
+++ components/Links.js
@@ -2,21 +2,26 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-function Links({ href, src, dimensions }) {
+function Links({ href, src: Component }) {
   return (
     <Link passHref href={href}>
       <a
         style={{
-          marginLeft: '4.4rem',
+          marginLeft: '6rem',
         }}
         target='_blank'
       >
-        <Image
-          alt=''
-          src={src}
-          width={dimensions.width}
-          height={dimensions.height}
-        />
+        {typeof Component !== 'string' ? (
+          <Component
+            style={{
+              width: '24px',
+              height: '24px',
+              fill: 'white',
+            }}
+          ></Component>
+        ) : (
+          <Image alt='' src={Component} width='23px' height='23px' />
+        )}
       </a>
     </Link>
   );
